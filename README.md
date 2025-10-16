9dee2db2337327108b645184536328862c08376e
Tomas Baran <tomas.baran@gmail.com>
2025-10-16 13:44:34 -0600

    config: Install Flutter & Install Firebase for two envs: dev & prod

---
+++ README.md
@@ -0,0 +1,3 @@
+# app_heydia_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/app_heydia_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_heydia_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
